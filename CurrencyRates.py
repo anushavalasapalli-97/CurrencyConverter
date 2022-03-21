33d99ef72b440e897b2419b513c19061c61eed19
anushavalasapalli-97 <56216071+anushavalasapalli-97@users.noreply.github.com>
2022-03-21 10:39:57 -0700

    Update CurrencyRates.py
    deleted currency conversion code

--- CurrencyRates.py
+++ CurrencyRates.py
@@ -1,22 +1,13 @@
-from forex_python.converter import CurrencyRates
 from forex_python.bitcoin import BtcConverter
-currency = CurrencyRates()
+
 
 def currencyConversion():
-  option = input("""Choose the conversion option \n1 --> Currency to Currency \n2 --> Bitcoin to Currency \n """).upper()
-  print(option)
-  if option == '1':
-    from_Currrency = input("From Currency: ").upper()
-    amount = int(input("Enter the amount: "))
-    to_Currency = input("To Currency: ").upper()
-    print(amount, from_Currrency, "to", to_Currency)
-    result = currency.convert(from_Currrency, to_Currency, amount)
-    print("Conversion Amount:", result)
-  elif option == '2':
-    b = BtcConverter()
-    input_Currrency = input("Enter Currency: ").upper()
-    result = b.get_latest_price(input_Currrency)
-    print("1 Bitcoin = "+str(result)+ ' '+ input_Currrency)
+
+  print("Converting 1 Bitcoin price to Currency")
+  b = BtcConverter()
+  input_Currrency = input("Enter Currency: ").upper()
+  result = b.get_latest_price(input_Currrency)
+  print("1 Bitcoin = "+str(result)+ ' '+ input_Currrency)
 
 
 if __name__ == '__main__':
